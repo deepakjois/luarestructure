05b19d64233fa7e49d26feca74aa9e4b166f2048
Deepak Jois <deepak.jois@gmail.com>
2017-06-02 13:53:25 +0530

    Working LazyArray

--- src/restructure/init.lua
+++ src/restructure/init.lua
@@ -3,7 +3,7 @@ local exports = {}
 exports.EncodeStream = require ('restructure.EncodeStream')
 exports.DecodeStream = require('restructure.DecodeStream')
 exports.Array = require('restructure.Array')
--- exports.LazyArray       = require './src/LazyArray'
+exports.LazyArray = require('restructure.LazyArray')
 exports.Bitfield = require('restructure.Bitfield')
 exports.Boolean = require('restructure.Boolean')
 exports.Buffer = require('restructure.Buffer')
@@ -14,8 +14,6 @@ exports.String = require('restructure.String')
 -- exports.Struct          = require './src/Struct'
 -- exports.VersionedStruct = require './src/VersionedStruct'
 
-
-
 local Number = require("restructure.Number")
 
 for k,v in pairs(Number) do
@@ -24,6 +22,5 @@ end
 
 -- for key, val of require './src/Pointer'
 --   exports[key] = val
-
 
 return exports
